a11ab25cc5fd0023b7d1ba4916e9f8c8ae46b129
Shisei Hanai <ruimo.uno@gmail.com>
2018-09-10 17:15:18 +0900

    refs #37

--- src/main/scala/com/ruimo/forms/common/OcrSettings.scala
+++ src/main/scala/com/ruimo/forms/common/OcrSettings.scala
@@ -40,6 +40,62 @@ private case class TesseractAcceptCharsImpl(
   val union: String = chars.map(_.chars).foldLeft(new StringBuilder)(_.append(_)).append(custom).toString.distinct
 }
 
+trait TegakiAcceptChars {
+  def isHiragana: Boolean
+  def isKatakan: Boolean
+  def isKanji: Boolean
+  def isNumber: Boolean
+  def isUpperAlpha: Boolean
+  def isLowerAlpha: Boolean
+  def isSymbol: Boolean
+}
+
+object TegakiAcceptChars {
+  def apply(
+    isHiragana: Boolean,
+    isKatakan: Boolean,
+    isKanji: Boolean,
+    isNumber: Boolean,
+    isUpperAlpha: Boolean,
+    isLowerAlpha: Boolean,
+    isSymbol: Boolean
+  ): TegakiAcceptChars = TegakiAcceptCharsImpl(isHiragana, isKatakan, isKanji, isNumber, isUpperAlpha, isLowerAlpha, isSymbol)
+
+  implicit object tegakiAcceptCharsFormat extends Format[TegakiAcceptChars] {
+    override def reads(jv: JsValue): JsResult[TegakiAcceptChars] = JsSuccess(
+      TegakiAcceptChars(
+        (jv \ "isHiragana").as[Boolean],
+        (jv \ "isKatakan").as[Boolean],
+        (jv \ "isKanji").as[Boolean],
+        (jv \ "isNumber").as[Boolean],
+        (jv \ "isUpperAlpha").as[Boolean],
+        (jv \ "isLowerAlpha").as[Boolean],
+        (jv \ "isSymbol").as[Boolean]
+      )
+    )
+
+    override def writes(f: TegakiAcceptChars): JsValue = Json.obj(
+      "isHiragana" -> f.isHiragana,
+      "isKatakan" -> f.isKatakan,
+      "isKanji" -> f.isKanji,
+      "isNumber" -> f.isNumber,
+      "isUpperAlpha" -> f.isUpperAlpha,
+      "isLowerAlpha" -> f.isLowerAlpha,
+      "isSymbol" -> f.isSymbol
+    )
+  }
+}
+
+private case class TegakiAcceptCharsImpl(
+  isHiragana: Boolean,
+  isKatakan: Boolean,
+  isKanji: Boolean,
+  isNumber: Boolean,
+  isUpperAlpha: Boolean,
+  isLowerAlpha: Boolean,
+  isSymbol: Boolean
+) extends TegakiAcceptChars
+
 sealed trait OcrSettings
 
 sealed trait TesseractLang {
@@ -153,6 +209,41 @@ object GoogleOcrSettings {
   }
 }
 
+trait TegakiOcrSettings extends OcrSettings {
+  def useLangModel: Boolean
+  def isMultiLine: Boolean
+  def acceptChars: TegakiAcceptChars
+}
+
+object TegakiOcrSettings {
+  def apply(
+    useLangModel: Boolean,
+    isMultiLine: Boolean,
+    acceptChars: TegakiAcceptChars
+  ): TegakiOcrSettings = TegakiOcrSettingsImpl(useLangModel, isMultiLine, acceptChars)
+
+  implicit object tegakiOcrSettingsFormat extends Format[TegakiOcrSettings] {
+    override def reads(jv: JsValue): JsResult[TegakiOcrSettings] = JsSuccess(
+      TegakiOcrSettings(
+        (jv \ "useLangModel").as[Boolean],
+        (jv \ "isMultiLine").as[Boolean],
+        (jv \ "acceptChars").as[TegakiAcceptChars]
+      )
+    )
+
+    override def writes(f: TegakiOcrSettings): JsValue = Json.obj(
+      "engine" -> "tegaki",
+      "useLangModel" -> f.useLangModel,
+      "isMultiLine" -> f.isMultiLine,
+      "acceptChars" -> (
+        f.acceptChars match {
+          case ta: TegakiAcceptCharsImpl => ta
+        }
+      )
+    )
+  }
+}
+
 private case class TesseractOcrSettingsImpl(
   lang: TesseractLang,
   acceptChars: TesseractAcceptChars
@@ -162,12 +253,19 @@ private case class GoogleOcrSettingsImpl(
   lang: GoogleOcrLang
 ) extends GoogleOcrSettings
 
+private case class TegakiOcrSettingsImpl(
+  useLangModel: Boolean,
+  isMultiLine: Boolean,
+  acceptChars: TegakiAcceptChars
+) extends TegakiOcrSettings
+
 object OcrSettings {
   implicit object ocrSettingsFormat extends Format[OcrSettings] {
     override def reads(jv: JsValue): JsResult[OcrSettings] = {
       (jv \ "engine").as[String] match {
         case "tesseract" => JsSuccess(jv.as[TesseractOcrSettings])
         case "google" => JsSuccess(jv.as[GoogleOcrSettings])
+        case "tegaki" => JsSuccess(jv.as[TegakiOcrSettings])
         case that: String => JsError(JsPath \ "engine", "Unknown OCR engine name '" + that + "'")
       }
     }
@@ -180,6 +278,18 @@ object OcrSettings {
             case ta: TesseractAcceptCharsImpl => ta
           }
         )
+      )
+
+      case teos: TegakiOcrSettings => Json.obj(
+        "useLangModel" -> teos.useLangModel,
+        "isMultiLine" -> teos.isMultiLine,
+        "isHiragana" -> teos.acceptChars.isHiragana,
+        "isKatakan" -> teos.acceptChars.isKatakan,
+        "isKanji" -> teos.acceptChars.isKanji,
+        "isNumber" -> teos.acceptChars.isNumber,
+        "isUpperAlpha" -> teos.acceptChars.isUpperAlpha,
+        "isLowerAlpha" -> teos.acceptChars.isLowerAlpha,
+        "isSymbol" -> teos.acceptChars.isSymbol
       )
 
       case gos: GoogleOcrSettings => Json.obj(
