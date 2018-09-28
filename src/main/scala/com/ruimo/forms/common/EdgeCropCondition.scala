d626749b379ca64d56fd3565cca7bc48fa54da8a
Shisei Hanai <ruimo.uno@gmail.com>
2018-09-28 15:40:56 +0900

    Black edge crop condition.

--- src/main/scala/com/ruimo/forms/common/EdgeCropCondition.scala
+++ src/main/scala/com/ruimo/forms/common/EdgeCropCondition.scala
@@ -2,7 +2,7 @@ package com.ruimo.forms.common
 
 import com.ruimo.graphics.twodim.Area
 import com.ruimo.scoins.Percent
-import play.api.libs.json.JsObject
+import play.api.libs.json.{JsArray, JsNumber, JsObject, Json}
 
 // 0 - 254
 final class EdgeCropSensivity(val value: Int) extends AnyVal {
@@ -39,3 +39,37 @@ trait ColorEdgeCropCondition extends EdgeCropCondition {
   def v: Percent
   def errorPercentage: Percent
 }
+
+case class BlackEdgeCropConditionImpl(
+  topArea: Option[Area],
+  bottomArea: Option[Area],
+  leftArea: Option[Area],
+  rightArea: Option[Area],
+  topSensivity: EdgeCropSensivity,
+  bottomSensivity: EdgeCropSensivity,
+  rightSensivity: EdgeCropSensivity,
+  leftSensivity: EdgeCropSensivity
+) extends BlackEdgeCropCondition {
+  lazy val asJson: JsObject = {
+    def areaToJson(area: Area) = {
+      JsArray(
+        Seq(
+          JsNumber(area.x.value), JsNumber(area.y.value), JsNumber(area.w.value), JsNumber(area.h.value)
+        )
+      )
+    }
+
+    JsObject(
+      topArea.map { a => "top" -> areaToJson(a) }.toSeq ++
+      bottomArea.map { a => "bottom" -> areaToJson(a) }.toSeq ++
+      leftArea.map { a => "left" -> areaToJson(a) }.toSeq ++
+      rightArea.map { a => "right" -> areaToJson(a) }.toSeq
+    ) ++ Json.obj(
+      "topSensivity" -> JsNumber(topSensivity.value),
+      "bottomSensivity" -> JsNumber(bottomSensivity.value),
+      "leftSensivity" -> JsNumber(leftSensivity.value),
+      "rightSensivity" -> JsNumber(rightSensivity.value)
+    )
+  }
+}
+
